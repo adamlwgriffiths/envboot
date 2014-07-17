016a602a1ef2b50bf63e33e870f57e2071691ee0
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-07-18 00:04:54 +1000

    Add dropbox

---
+++ dropbox/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd $DIR
+
+FILENAME="DropboxInstaller.dmg"
+DMG="Dropbox Installer"
+
+wget -O "./$FILENAME" "https://www.dropbox.com/download?src=index&plat=mac"
+
+hdiutil mount "$FILENAME"
+
+open "/Volumes/$DMG/Chromium.app"
+
+hdiutil unmount "/Volumes/$DMG"
+
+rm "./$FILENAME"
+
+popd
+
