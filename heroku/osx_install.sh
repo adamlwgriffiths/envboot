f8c9bdd9660b5d849be2c5ab3d60e5b083fa5586
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-07-20 19:38:59 +1000

    Add heroku login command.

--- heroku/osx_install.sh
+++ heroku/osx_install.sh
@@ -6,6 +6,8 @@ pushd $DIR
 
 wget -qO- https://toolbelt.heroku.com/install.sh | sh
 
+/usr/local/heroku/bin/heroku login
+
 if [ -d ~/.bashrc_scripts ]; then
     cp ./bashrc_scripts/heroku.sh ~/.bashrc_scripts
     chmod +x ~/.bashrc_scripts/heroku.sh
