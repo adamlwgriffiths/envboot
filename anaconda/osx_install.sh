51aaa63a76279c6d2bfb99797e0a8c5ab52e3448
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-07-21 17:10:55 +1000

    Use $FILENAME in the download params.

--- anaconda/osx_install.sh
+++ anaconda/osx_install.sh
@@ -6,7 +6,7 @@ pushd $DIR
 
 FILENAME="Anaconda-2.0.1-MacOSX-x86_64.sh"
 
-wget -O "./$FILENAME" "http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-2.0.1-MacOSX-x86_64.sh" -c
+wget -O "./$FILENAME" "http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/$FILENAME" -c
 
 chmod +x "./$FILENAME"
 "./$FILENAME"
