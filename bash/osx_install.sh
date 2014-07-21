5d4de334ff45e51759f456e284a658c077f21272
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-07-21 17:12:36 +1000

    Check for existing bashrc/bash_profile and bak them.

--- bash/osx_install.sh
+++ bash/osx_install.sh
@@ -5,10 +5,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $DIR
 
 # Add to ~. Makes DEL key work in OS-X python terminal.
-cp ./.bashrc ~
+if [ -f ~/.bashrc ]; then
+    mv ~/.bashrc ~/.bashrc.bak
+fi
+cp ./.bashrc ~/.bashrc
 mkdir -p ~/.bashrc_scripts
 
 # link .bash_profile to .bashrc
+if [ -f ~/.bash_profile ]; then
+    mv ~/.bash_profile ~/.bash_profile.bak
+fi
 ln -s ~/.bashrc ~/.bash_profile
 
 popd
