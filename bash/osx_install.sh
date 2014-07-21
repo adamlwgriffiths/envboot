f9ebf7dc205a69622b9a6a36c191edc3e228df90
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-07-21 17:35:52 +1000

    Remove invalid comment

--- bash/osx_install.sh
+++ bash/osx_install.sh
@@ -4,7 +4,6 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $DIR
 
-# Add to ~. Makes DEL key work in OS-X python terminal.
 if [ -f ~/.bashrc ]; then
     mv ~/.bashrc ~/.bashrc.bak
 fi
