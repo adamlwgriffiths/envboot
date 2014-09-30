b33bcd4de9859e18f08cbe77044e68f3dadf9770
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-09-30 15:42:39 +1000

    cd into bash dir

--- bash/osx_base.sh
+++ bash/osx_base.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -e
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd $DIR
+
 if [ -d ~/.bash_scripts ]; then
     cp ./bash_scripts/library.sh ~/.bash_scripts
     chmod +x ~/.bash_scripts/library.sh
 fi
 
+popd
