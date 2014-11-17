e65d72e54b375309d56da1add4a034412de97fc8
Adam Griffiths <adam.lw.griffiths@gmail.com>
2014-11-17 14:31:55 +1100

    Add color for terminal colours

--- bash/osx_base.sh
+++ bash/osx_base.sh
@@ -7,6 +7,9 @@ pushd $DIR
 if [ -d ~/.bash_scripts ]; then
     cp ./bash_scripts/library.sh ~/.bash_scripts
     chmod +x ~/.bash_scripts/library.sh
+
+    cp ./bash_scripts/color.sh ~/.bash_scripts
+    chmod +x ~/.bash_scripts/color.sh
 fi
 
 popd
--- bash/ubuntu_base.sh
+++ bash/ubuntu_base.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd $DIR
+
+if [ -d ~/.bash_scripts ]; then
+    cp ./bash_scripts/color.sh ~/.bash_scripts
+    chmod +x ~/.bash_scripts/color.sh
+fi
 
 # set vi to default editor
 sudo update-alternatives --set editor /usr/bin/vim.basic
